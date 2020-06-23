78e98a2556a3aa3370b3ff593de2d586fc8a027f
Miguel <miguel.lurgi@gmail.com>
2020-06-23 17:09:44 +0100

    Added lesson 5

--- code.r
+++ code.r
@@ -31,7 +31,81 @@ install.packages("igraph")
 library(igraph)
 
 
-srequire(cheddar)
+require(igraph)
+# Ten species
+species <- 1:10
+network <- graph.empty() + vertices(species)
+
+# Link between species 5 and 7
+network[5,7] <- 1
+
+network <- as.directed(random.graph.game(10, .3), mode = 'arbitrary')
+
+
+# number of species
+S <- vcount(network)
+
+# number of interactions
+L <- ecount(network)
+
+# average number of interactions species
+L.S <- L/S
+
+# food web connectance
+C <- L/S^2
+
+plot(network)
+
+# Convert Benguela adjacency matrix to an igraph network
+benguela.network <- graph.adjacency(benguela.AM)
+
+# Plot the Benguela food web
+plot(benguela.network, edge.arrow.size = 0.2)
+
+# Calculate connectance
+connectance <- ecount(benguela.network) / vcount(benguela.network)^2 
+
+# Print connectance va
+print(paste0('Connectance of Benguela network =', round(connectance,2)))
+
+# Calculate number of links per species
+links.per.species <- ecount(benguela.network) / vcount(benguela.network)
+
+
+# Calculate conectance of anemoref
+bipartite.connectance <- sum(anemonef) / (dim(anemonef)[1] * dim(anemonef)[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+require(cheddar)
 data(Benguela)
 
 require(igraph)
@@ -40,7 +114,5 @@ write.graph(g, file='benguela.edgelist', format='edgelist')
 
 write.table(PredationMatrix(Benguela), file = 'benguela.adjmat')
 
-
-benguela.EL <- read.table()
 
 
